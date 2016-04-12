fb131a0ae8d9a3b81a68700e8d4ee41cec3f6273
Marco Ustarroz <marcoustarroz@gmail.com>
2016-04-12 09:37:35 -0300

    no se modificó el ejercicio 4 , solo se le agrego la clase utils y contants para ordenar, pero no se utilizan en el codigo del ejercicio.

---
+++ src/main/java/com/globant/bootcamp/tae/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.globant.bootcamp.tae.utils;
+
+public final class Constants {
+
+	public static final String TEMA1_EJ2_URL = "http://live.com";
+	public static final String TEMA1_EJ2_PALABRA = "support.com";
+	public static final String TEMA1_EJ2_USER = "alkimer@hotmail.com";
+	public static final String TEMA1_EJ2_PASS = "inicio01";
+	
+	public static final String TEMA1_EJ3_URL = "http://google.com";
+	public static final String TEMA1_EJ3_TEXTO_BUSQUEDA = "Constant Concept";
+
+}
